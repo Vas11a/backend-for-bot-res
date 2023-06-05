a952d1543240426dcc6d75e53c7f20450c7de46a
Vasia Panov <vasapanov721@gmail.com>
2023-06-05 10:55:38 +0200

    change start project command

--- main.py
+++ main.py
@@ -62,5 +62,5 @@ app.add_middleware(
 
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=port)
