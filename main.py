ee7128b6f1c0ff65b761b1b4be6f30def504fd6d
Vasia Panov <vasapanov721@gmail.com>
2023-06-05 11:01:42 +0200

    bug fixed

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import uvicorn
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 app = FastAPI()
 port = int(os.environ.get("PORT", 8000))
 
@@ -64,3 +62,5 @@ app.add_middleware(
 
 # if __name__ == "__main__":
 #     uvicorn.run("main:app", host="0.0.0.0", port=port)
+
+uvicorn main:app --host 0.0.0.0 --port 10000
